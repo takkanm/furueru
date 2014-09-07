7a8288729d9f41598aab2e1d1c4571d79649fe97
Mitsutaka Mimura <takkanm@gmail.com>
2014-09-07 16:59:38 +0900

    go fmt

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"os"
 	"image"
+	"image/color/palette"
 	"image/gif"
 	"image/png"
-	"image/color/palette"
 	"math/rand"
- )
+	"os"
+)
 
 func writeGifFile(outFileNmae string, g gif.GIF) {
 	outFile, _ := os.Create(outFileNmae)
 	defer outFile.Close()
-
 
 	g.Delay = make([]int, len(g.Image))
 	g.LoopCount = 0
@@ -20,15 +19,15 @@ func writeGifFile(outFileNmae string, g gif.GIF) {
 	gif.EncodeAll(outFile, &g)
 }
 
-func slideImage (in image.Image, slideX int, slideY int) *image.Paletted {
+func slideImage(in image.Image, slideX int, slideY int) *image.Paletted {
 	margin := 2
 	inBounds := in.Bounds()
-	rect :=  image.Rect(inBounds.Min.X, inBounds.Min.Y, inBounds.Max.X + margin, inBounds.Max.Y + margin)
+	rect := image.Rect(inBounds.Min.X, inBounds.Min.Y, inBounds.Max.X+margin, inBounds.Max.Y+margin)
 	pl := image.NewPaletted(rect, palette.WebSafe)
 
 	for x := 0; x < inBounds.Max.X; x++ {
 		for y := 0; y < inBounds.Max.Y; y++ {
-			pl.Set(x + slideX, y + slideY, in.At(x, y))
+			pl.Set(x+slideX, y+slideY, in.At(x, y))
 		}
 	}
 	return pl
@@ -41,14 +40,14 @@ func calsSlideVolume() (int, int) {
 }
 
 func generateAnimeGif(inFileNmae string) {
-	inFile,  _ := os.Open(inFileNmae)
+	inFile, _ := os.Open(inFileNmae)
 	defer inFile.Close()
 
 	pngImage, _ := png.Decode(inFile)
 	moveTimes := 100
 
-	outGif := gif.GIF {
-		Image: []*image.Paletted {},
+	outGif := gif.GIF{
+		Image: []*image.Paletted{},
 	}
 
 	for s := 0; s < moveTimes; s++ {
@@ -60,9 +59,9 @@ func generateAnimeGif(inFileNmae string) {
 		outGif.Image = append(outGif.Image, palet)
 	}
 
-	writeGifFile(inFileNmae + ".gif", outGif)
+	writeGifFile(inFileNmae+".gif", outGif)
 }
 
-func main () {
+func main() {
 	generateAnimeGif(os.Args[1])
 }
